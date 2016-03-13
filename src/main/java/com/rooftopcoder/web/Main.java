93c97b8d27ef142c6d88bf38fd8de130d7d350da
jeyrs <jeyrschabu@gmail.com>
2016-03-12 22:01:28 -0800

    set up to get configs from environment

--- src/main/java/com/rooftopcoder/web/Main.java
+++ src/main/java/com/rooftopcoder/web/Main.java
@@ -1,6 +1,8 @@
 package com.rooftopcoder.web;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.rooftopcoder.web.data.MongoConnectionConfig;
 import com.rooftopcoder.web.resources.HomeResource;
 import com.rooftopcoder.web.resources.ProductCategoryResource;
@@ -10,6 +12,9 @@ import com.rooftopcoder.web.services.ProductCategoryService;
 import com.rooftopcoder.web.services.ProductFeatureService;
 import com.rooftopcoder.web.services.ProductService;
 import spark.servlet.SparkApplication;
+import static com.rooftopcoder.web.constants.ApplicationConstants.*;
+
+import java.util.Arrays;
 
 public class Main implements SparkApplication{
 
@@ -18,11 +23,24 @@ public class Main implements SparkApplication{
     }
 
     private static void initWithRoutes() {
-        final MongoConnectionConfig mongoConnectionConfig = new MongoConnectionConfig(new MongoClient(), "shadow-rigs");
+        String database = (System.getenv(MONGO_DATABASE) == null) ? DEFAULT_DB : System.getenv(MONGO_DATABASE);
+        String mongoHost = (System.getenv(MONGO_HOST) == null) ? DEFAULT_HOST : System.getenv(MONGO_HOST);
+        String userName = System.getenv(MONGO_USERNAME);
+        String password = System.getenv(MONGO_PASSWORD);
 
-        final ProductCategoryService categoryService = new ProductCategoryService().setDataProvider(mongoConnectionConfig);
-        final ProductService productService = new ProductService().setDataProvider(mongoConnectionConfig);
-        final ProductFeatureService featureService = new ProductFeatureService().setDataProvider(mongoConnectionConfig);
+        MongoClient mongoClient = new MongoClient();
+
+        if (userName != null && password != null) {
+            MongoCredential credential = MongoCredential.createCredential(userName,
+                    database, password.toCharArray());
+            mongoClient = new MongoClient(new ServerAddress(mongoHost), Arrays.asList(credential));
+        }
+
+        final MongoConnectionConfig dbCfg = new MongoConnectionConfig(mongoClient, database);
+
+        final ProductCategoryService categoryService = new ProductCategoryService().setDataProvider(dbCfg);
+        final ProductService productService = new ProductService().setDataProvider(dbCfg);
+        final ProductFeatureService featureService = new ProductFeatureService().setDataProvider(dbCfg);
 
         new ProductResource(productService, categoryService);
         new ProductCategoryResource(categoryService);
--- src/main/java/com/rooftopcoder/web/constants/ApplicationConstants.java
+++ src/main/java/com/rooftopcoder/web/constants/ApplicationConstants.java
@@ -4,4 +4,10 @@ package com.rooftopcoder.web.constants;
 public interface ApplicationConstants {
     String CONTEXT = "/v1";
     String MODELS_PACKAGE = "com.rooftopcoder.web.models";
+    String MONGO_DATABASE = "MONGO_DB";
+    String MONGO_HOST = "MONGO_HOST";
+    String MONGO_USERNAME = "MONGO_USERNAME";
+    String MONGO_PASSWORD = "MONGO_PASSWORD";
+    String DEFAULT_DB = "shadow-rigs";
+    String DEFAULT_HOST = "localhost";
 }
