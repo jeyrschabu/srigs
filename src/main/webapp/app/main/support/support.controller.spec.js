84445fc0c77514850acd8258d29b6e83edfe618d
jeyrs <jeyrschabu@gmail.com>
2016-07-24 14:02:44 -0700

    more tests

--- src/main/webapp/app/main/support/support.controller.spec.js
+++ src/main/webapp/app/main/support/support.controller.spec.js
@@ -1,25 +1,23 @@
-// /**
-//  * Created by jeyrschabu on 6/20/16.
-//  */
-//
-// describe('Home', function() {
-//     var scope;
-//     var supportController;
-//
-//     beforeEach(function () {
-//         module('Home');
-//     });
-//
-//     describe('SupportController', function() {
-//         beforeEach(inject(function($rootScope, $controller){
-//             scope = $rootScope.$new();
-//             supportController = $controller('SupportController', {
-//                 '$scope' : scope
-//             });
-//         }));
-//
-//         it('should have a title to match the page', function(){
-//             expect(scope.pageTitle).toBe('Support');
-//         });
-//     })
-// });
+/**
+ * Created by jeyrschabu on 6/20/16.
+ */
+
+describe('Support', function(){
+    var scope,
+        supportController;
+
+    beforeEach(module('Rigs'));
+
+    describe('SupportController', function() {
+
+        beforeEach(inject(function($rootScope, $controller) {
+            scope = $rootScope.$new();
+            supportController = $controller('SupportController', { '$scope' : scope });
+        }));
+
+        it ('should have a title to match the page', function() {
+            expect(supportController.pageTitle).toBe('Support');
+            expect(scope.bodyClass).toBe(supportController.pageTitle.toLocaleLowerCase());
+        });
+    })
+});
