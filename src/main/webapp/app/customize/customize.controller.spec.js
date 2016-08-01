b7d1eba9a390f2b95585d798dbf16565dd848510
jeyrs <jeyrschabu@gmail.com>
2016-08-01 11:56:45 -0700

    added a new test and a general way to get rig options

--- src/main/webapp/app/customize/customize.controller.spec.js
+++ src/main/webapp/app/customize/customize.controller.spec.js
@@ -4,20 +4,61 @@
 
 describe('Customize', function() {
     var scope,
-        customizeController;
+        customizeController,
+        mockProductService;
 
     beforeEach(module('Rigs'));
 
     describe('CustomizeController', function() {
 
-        beforeEach(inject(function($rootScope, $controller) {
+        beforeEach(inject(function($rootScope, $controller, ProductService) {
             scope = $rootScope.$new();
-            customizeController = $controller('CustomizeController', { '$scope' : scope });
+            mockProductService = ProductService
+            customizeController = $controller('CustomizeController', {
+                '$scope' : scope,
+                'ProductService' : mockProductService
+            });
         }));
 
         it ('should have a title to match the page', function() {
             expect(customizeController.pageTitle).toBe('Customize');
             expect(scope.bodyClass).toBe(customizeController.pageTitle.toLocaleLowerCase());
+        });
+
+        it ('should have a default case option from the product mark when initialized', function() {
+            var product = {
+                name: 'Phantom',
+                price: 1060,
+                specs : [
+                    { name : 'Corsair Obsidian 450D', type : 'Case'},
+                    { name : 'Corsair Vengeance C70 Military Green', type : 'Case' }
+                ],
+                marks:[
+                    {
+                        price: 1060,
+                        name : 'Mark 1',
+                        brand : 'Intel',
+                        specs : [
+                            { name : 'Corsair Obsidian 450D', type : 'Case' },
+                            { name : 'Intel FX-6300', type : 'CPU' }
+                        ]
+                    }
+                ]
+
+            };
+
+            spyOn(mockProductService, 'findById').and.callFake(function() {
+                return {
+                    then: function(callback) { return callback({data: product}); }
+                };
+            });
+
+
+            customizeController.initialize('id');
+            expect(mockProductService.findById).toHaveBeenCalled();
+            expect(customizeController.rig.caseOptions).toBeDefined();
+            expect(customizeController.rig.caseOptions.current.name).toBe(product.marks[0].specs[0].name);
+
         });
     })
 });
