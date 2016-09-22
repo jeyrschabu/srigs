85eb3b39f8cb35cda90ca4ae1c4af0dcaa160d97
jeyrs <jeyrschabu@gmail.com>
2016-09-21 22:58:51 -0700

    misc clean

--- src/main/java/com/rooftopcoder/web/Main.java
+++ src/main/java/com/rooftopcoder/web/Main.java
@@ -31,9 +31,9 @@ public class Main implements SparkApplication {
         log.info("Initializing database");
 
         String database  = serverConfig.db() == null ? DEFAULT_DB : serverConfig.db();
-        String mongoHost = serverConfig.dbHost() == null ? System.getProperty("MONGO_HOST") : serverConfig.dbHost();
-        String userName  = serverConfig.dbUser() == null ? System.getProperty("MONGO_USERNAME"): serverConfig.dbUser();
-        String password  = serverConfig.dbPass() == null ? System.getProperty("MONGO_PASSWORD"): serverConfig.dbPass();;
+        String mongoHost = serverConfig.dbHost() == null ? System.getProperty(MONGO_HOST) : serverConfig.dbHost();
+        String userName  = serverConfig.dbUser() == null ? System.getProperty(MONGO_USERNAME): serverConfig.dbUser();
+        String password  = serverConfig.dbPass() == null ? System.getProperty(MONGO_PASSWORD): serverConfig.dbPass();;
 
         log.info("Connecting to {}", database);
 
--- src/main/java/com/rooftopcoder/web/constants/ApplicationConstants.java
+++ src/main/java/com/rooftopcoder/web/constants/ApplicationConstants.java
@@ -4,10 +4,8 @@ package com.rooftopcoder.web.constants;
 public interface ApplicationConstants {
     String CONTEXT = "/api/v1";
     String MODELS_PACKAGE = "com.rooftopcoder.web.models";
-    String MONGO_DATABASE = "MONGO_DB";
     String MONGO_HOST = "MONGO_HOST";
     String MONGO_USERNAME = "MONGO_USERNAME";
     String MONGO_PASSWORD = "MONGO_PASSWORD";
     String DEFAULT_DB = "shadow-rigs";
-    String DEFAULT_HOST = "localhost";
 }
